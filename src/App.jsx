b8cc62e8e3a6ff805e2c8b48653cb30c90781103
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-10-13 12:16:28 +0100

    capture unique categories from data

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import menu from "./data";
 import Menu from "./Menu";
 import Title from "./Title";
 
+// LONG FORM VERSION
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ["all", ...tempSet];
+// console.log(tempItems);
+
+// ONE-LINER
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories);
 
   return (
     <main>
