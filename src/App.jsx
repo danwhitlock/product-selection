a062029329d220ce4ac343a70bd1d049791cb529
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-10-13 12:20:10 +0100

    create category component & buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import menu from "./data";
 import Menu from "./Menu";
 import Title from "./Title";
+import Categories from "./Categories";
 
 // LONG FORM VERSION
 // const tempCategories = menu.map((item) => item.category);
@@ -15,12 +16,12 @@ const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
-  console.log(categories);
 
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
---
+++ src/Categories.jsx
@@ -0,0 +1,14 @@
+const Categories = ({ categories }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category) => {
+        return (
+          <button className="btn" type="button" key={category}>
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+export default Categories;
